8229a1a1d03a26cf7ac56f2454c5f610cbccba51
Khalil Mubarak <khalil.f.mubarak@gmail.com>
2022-11-25 23:26:34 -0500

    Refactored findWaldo to use forEach

--- callbacks-intro/findWaldo.js
+++ callbacks-intro/findWaldo.js
@@ -1,11 +1,6 @@
 // The second argument/parameter is expected to be a (callback) function
 const findWaldo = function(names, found) {
-  for (let i = 0; i < names.length; i++) {
-    let name = names[i];
-    if (name === "Waldo") {
-      found(i);   // execute callback
-    }
-  }
+  names.forEach( function(element, index){ if (element === "Waldo") { found(index); } });
 }
 
 const actionWhenFound = function(index) {
